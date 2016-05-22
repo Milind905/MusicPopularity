cc982b8f85abc0506efbf56a796be0fd362819d7
Milind Shah <milindS.905@gmail.com>
2016-05-21 23:44:14 -0700

    Fixed search results

--- public/scripts/controllers/loggedinController.js
+++ public/scripts/controllers/loggedinController.js
@@ -6,6 +6,7 @@ angular.module('musicPopularity')
 	self.searchBox = null;
 	self.searchResults = null;
 	self.modSearchResults = {};
+	self.topSearchResults = [];
 
 	self.obtainTokens = function() {
 		return spotify.storeTokens($stateParams.access_token, $stateParams.refresh_token);
@@ -28,11 +29,12 @@ angular.module('musicPopularity')
 		}
 		spotify.getMultAlbumInfo(albumIds.toString()).then(function(res){
 			modAlbumInfo(res);
-			modArtistInfo(res);
+			modArtistInfo();
+			modTrackInfo();
+			getTopTen();
 		}, function(err){
 			//do something
-		}).then(function(res){
-					});
+		});
 	}
 
     self.obtainTokens().then(function(){
@@ -43,9 +45,12 @@ angular.module('musicPopularity')
 		//do something
 	});
 
-	/***** Helper Functions *****/
+
+
+	/**********************
+		Helper Functions 
+	***********************/
 	function modAlbumInfo(res){
-		var deferred = $q.defer();
 		var albumInfo = [];
 		var artists;
 		var tracks;
@@ -71,13 +76,88 @@ angular.module('musicPopularity')
 				name: res.data.albums[i].name,
 				popularity: res.data.albums[i].popularity,
 				release_date: res.data.albums[i].release_date,
-				tracks: tracks.toString()
+				tracks: tracks.toString(),
+				type: res.data.albums[i].type
 			} 
 		}
-
 		self.modSearchResults.albums = albumInfo;
-		deferred.resolve();
-		return deferred.promise;
+	}
+
+	function modArtistInfo(){
+		var artistInfo = [];
+
+		for(var i=0; i<self.searchResults.artists.items.length; i++){
+			artistInfo[i] = {
+				href: self.searchResults.artists.items[i].href,
+				id: self.searchResults.artists.items[i].id,
+				imageLarge: self.searchResults.artists.items[i].images[1],
+				imageMedium: self.searchResults.artists.items[i].images[2],
+				imageSmall: self.searchResults.artists.items[i].images[3],
+				name: self.searchResults.artists.items[i].name,
+				popularity: self.searchResults.artists.items[i].popularity,
+				type: self.searchResults.artists.items[i].type
+			}
+		}
+		self.modSearchResults.artists = artistInfo;	
+	}
+
+	function modTrackInfo() {
+		var trackInfo = [];
+		var artists;
+
+		for(var i=0; i<self.searchResults.tracks.items.length; i++){
+			artists = [];
+
+			for(var j=0; j<self.searchResults.tracks.items[i].artists.length; j++){
+				artists[j] = self.searchResults.tracks.items[i].artists[j].name;
+			}
+
+			trackInfo[i] = {
+				albumId: self.searchResults.tracks.items[i].album.id,
+				albumName: self.searchResults.tracks.items[i].album.name,
+				artists: artists.toString(),
+				duration: self.searchResults.tracks.items[i].duration_ms,
+				explicit: self.searchResults.tracks.items[i].explicit,
+				href: self.searchResults.tracks.items[i].href,
+				id: self.searchResults.tracks.items[i].id,
+				name: self.searchResults.tracks.items[i].name,
+				preview: self.searchResults.tracks.items[i].preview_url,
+				popularity: self.searchResults.tracks.items[i].popularity,
+				type: self.searchResults.tracks.items[i].type
+			}
+		}
+		self.modSearchResults.tracks = trackInfo;	
+	}
+
+	function getTopTen() {
+		var topTenItems = [];
+
+		for(var i=0; i<self.modSearchResults.tracks.length; i++){
+			topTenItems.push(self.modSearchResults.tracks[i]);
+		}
+		for(var i=0; i<self.modSearchResults.albums.length; i++){
+			topTenItems.push(self.modSearchResults.albums[i]);
+		}
+		for(var i=0; i<self.modSearchResults.artists.length; i++){
+			topTenItems.push(self.modSearchResults.artists[i]);
+		}
+		self.topSearchResults = sortOnPopularity(topTenItems);
+	}
+
+	function sortOnPopularity(inArray) {
+		var outArray = inArray.sort(comparePopularity);
+		outArray = outArray.splice(0, 10);
+		return outArray;
+	}
+
+	function comparePopularity(a, b){
+		if(a.popularity < b.popularity){
+			return 1;
+		} else if(a.popularity > b.popularity){
+			return -1;
+		} else {
+			return 0;
+		}
 	}
 
 }]);
